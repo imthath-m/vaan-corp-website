de04dc07ef5cad789f63fa7fa6beb7b9c6dea6dd
Asif <sheikasifofficial@gmail.com>
2023-02-04 14:31:29 +0530

    Update countdown privacy policy

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import BlogFullPage from './Components/Blogs/BlogFullPage';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import HomeContainer from './Components/Home/HomeContainer';
+import CountDownPrivacyPolicy from './Components/Privacy/CoutDownPrivacyPolicy';
 
 function App() {
   const home = useSelector(state => state.mainState.home)
   return (
     <>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<HomeContainer />} />
-        <Route path='/blog' element={<BlogsContainer />} />
-        <Route path='/blogFull/:id' element={<BlogFullPage />} />
-      </Routes>
-      <Footer/>
+      {window.location.pathname.toLowerCase() != '/privacy/countdown' && <Header />}
+        <Routes>
+          <Route path='/' element={<HomeContainer />} />
+          {/* <Route path='/blog' element={<BlogsContainer />} />
+          <Route path='/blogFull/:id' element={<BlogFullPage />} /> */}
+          <Route path='/privacy/countdown' element={<CountDownPrivacyPolicy />} />
+        </Routes>
+        {window.location.pathname.toLowerCase() != '/privacy/countdown' && <Footer/>}
       </>
   );
 }
