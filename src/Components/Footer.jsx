21a347f1418d482882d589c5a6662026837ee28d
Asif <sheikasifofficial@gmail.com>
2023-05-27 20:56:41 +0530

    add footer links to navigate pages

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 // import twitter from "../assets/images/twitter.svg"
 // import toast, { Toaster } from 'react-hot-toast';
 import { twitterPage } from '../helper'
+import { useNavigate } from 'react-router-dom'
 
 function Footer() {
   // const [first, setFirst] = useState('');
@@ -37,8 +38,24 @@ function Footer() {
   //   setFirst('');
   //   // window.location.reload()
   // }
+
+  const navigate = useNavigate()
+
+  const navigateToSupport = () =>{
+    navigate('/support')
+  }
+  const navigateToTermsOfService = () =>{
+    navigate('/terms-of-service')
+  }
+  const navigateToPrivacyPolicy = () =>{
+    navigate('/privacy-policy')
+  }
+  const navigateToCancellation = () =>{
+    navigate('/cancellation')
+  }
+
   return (
-    <footer className='footer' id='footer' onClick={twitterPage}>
+    <footer className='footer' id='footer' >
       {/* <div className='footer_heading'>
         <h1 className='footer_heading-h'>Subscribe to our newsletter</h1>
         <p className='footer_heading-p'>Get latest news and update</p>
@@ -51,7 +68,13 @@ function Footer() {
         <button type='submit' className='footer_form-button'>Submit</button>
         <Toaster position="bottom-center" />
       </form> */}
-      <span className='footer-copyrights'>
+      <div className='footerLinks'>
+        <div onClick={navigateToSupport}>Support</div>
+        <div onClick={navigateToTermsOfService}>Terms-of-service</div>
+        <div onClick={navigateToPrivacyPolicy}>Privacy-policy</div>
+        <div onClick={navigateToCancellation}>Cancellation</div>
+      </div>
+      <span className='footer-copyrights' onClick={twitterPage}>
         {/* <a href='https://twitter.com/skydevz' target="_blank"><img className='footer-copyrights_img' alt='footer_twit_pic' src={twitter} /></a> */}
          © {new Date().getFullYear()}, Vaan Corporation. All Rights Reserved.</span>
     </footer>
